81b98f58fdbdcf72e5a185e097f37da7447cd0ea
Alasdair Casperd <alasdaircasperd@gmail.com>
2025-02-15 12:57:37 +0000

    Structure – Add icons to categories

--- src/data/category.data.tsx
+++ src/data/category.data.tsx
@@ -1,33 +1,47 @@
+import {
+  IconBallTennis,
+  IconGlobe,
+  IconHeart,
+  IconPalette,
+  IconScale,
+  IconTie,
+} from "@tabler/icons-react";
 import { Category } from "../types/category";
 
 const love: Category = {
   id: "love",
   name: "Love",
+  icon: <IconHeart />,
 };
 
 const travel: Category = {
   id: "travel",
   name: "Travel",
+  icon: <IconGlobe />,
 };
 
 const activity: Category = {
   id: "activity",
   name: "Activity",
+  icon: <IconBallTennis />,
 };
 
 const achievements: Category = {
   id: "achievements",
   name: "Achievements",
+  icon: <IconTie />,
 };
 
 const skills: Category = {
   id: "skills",
   name: "Skills",
+  icon: <IconPalette />,
 };
 
 const hardship: Category = {
   id: "hardship",
   name: "Hardship",
+  icon: <IconScale />,
 };
 
 export { love, travel, activity, achievements, skills, hardship };
--- src/types/category.ts
+++ src/types/category.ts
@@ -1,4 +1,7 @@
+import { JSX } from "react";
+
 export type Category = {
   id: string;
   name: string;
+  icon: JSX.Element;
 };
